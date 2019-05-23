f48124d0cc568b106d4e0f7adffaa8c563bf7464
Jack Atherton <jackrrye@gmail.com>
2019-05-23 02:46:30 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,7 +6,10 @@ def hello_t(array)
      i = i + 1 
     end
     array 
+  else 
+    puts "Hey! No block was given!"
   end
+end
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
